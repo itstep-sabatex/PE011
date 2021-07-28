a5fcbc2330e3f0ce00fb1f870352d06c4bcdbef7
Сергій Лакас <lakas_s@itstep.academy>
2021-07-28 21:19:45 +0300

    Add project files.

---
+++ PE011/DemoClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE011
+{
+    public class DemoClass
+    {
+        public int MaxPower;
+        private int _maxPower;
+        public int Add(ref string a,params object[] maxPower)
+        {
+            a = "Hello";
+            return 1;
+        }
+        public int Add(ref string a) => Add(ref a, 10);
+ 
+
+    }
+    public abstract class ADemoClass
+    {
+        public abstract int A { get; set; }
+    }
+    public interface IDemoClass
+    {
+        int A { get; set; }
+    }
+    public class B:IDemoClass
+    {
+        int a;
+        public int A { get=>a; set=>a=value; }
+    }
+
+
+
+}
---
+++ PE011/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace PE011
+{
+    class A
+    {
+        private int a=10;
+        public int B { get; set; }
+        public virtual int Test()
+        {
+            return a;
+        }
+    }
+    class B:A
+    {
+        public string GetStringFromObject(object o)=>o.ToString();
+        public int A { get; set; }
+        public override int Test()
+        {
+            return 25;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+
+            i =i + 10;
+
+            #region Виводимо на екран
+            Console.WriteLine("Hello World!");
+            #endregion
+
+            var b = new B();
+            b.Test();
+            var a = (A)b;
+            a.Test();
+        }
+    }
+}
